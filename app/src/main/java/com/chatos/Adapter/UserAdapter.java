a77427ae30c89fae0638a870ddf5bb7382fff0dc
Tombh6 <tombh6@cs.colman.ac.il>
2021-12-09 15:46:00 +0200

    Set Message Activity for getting Chat

--- app/src/main/java/com/chatos/Adapter/UserAdapter.java
+++ app/src/main/java/com/chatos/Adapter/UserAdapter.java
@@ -1,6 +1,7 @@
 package com.chatos.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.chatos.MessageActivity;
 import com.chatos.Model.User;
 import com.chatos.R;
 
@@ -45,6 +47,16 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
         } else {
             Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
         }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (mContext, MessageActivity.class);
+                intent.putExtra("userid",user.getId());
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
