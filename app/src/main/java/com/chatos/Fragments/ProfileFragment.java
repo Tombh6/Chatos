b9e432313d163e8827b4f3548fb387e1d0c40e65
Tombh6 <tombh6@cs.colman.ac.il>
2021-12-12 13:22:16 +0200

    Upload user's profile picture

--- app/src/main/java/com/chatos/Fragments/ProfileFragment.java
+++ app/src/main/java/com/chatos/Fragments/ProfileFragment.java
@@ -1,18 +1,39 @@
 package com.chatos.Fragments;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.chatos.Model.User;
 import com.chatos.R;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,27 +41,51 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import dmax.dialog.SpotsDialog;
 
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment<progressBar> extends Fragment {
+
+
+    FirebaseStorage storage;
+
+
 
     CircleImageView image_profile;
     TextView username;
 
+
     DatabaseReference reference;
     FirebaseUser fuser;
+
+    StorageReference storageReference;
+    private static final int IMAGE_REQUEST = 1;
+    private Uri imageUri;
+    private StorageTask uploadTask;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
 
         image_profile = view.findViewById(R.id.profile_image);
         username = view.findViewById(R.id.username);
+
+        storage = FirebaseStorage.getInstance();
+
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
 
         fuser = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("Users").child(fuser.getUid());
@@ -64,7 +109,92 @@ public class ProfileFragment extends Fragment {
         });
 
 
+        // open image
+        image_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Pass in the mime type you'd like to allow the user to select
+                // as the input
+                mGetContent.launch("image/*");
+
+            }
+        });
+
+
+
+
+
 
         return view;
     }
+
+
+
+    private String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContext().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri result) {
+                    if(result != null){
+
+                        image_profile.setImageURI(result);
+                        imageUri=result;
+                        //---------------------------
+                        if(imageUri != null){
+                            final StorageReference fileReference = storageReference.child(System.currentTimeMillis()
+                                    +"."+getFileExtension(imageUri));
+
+                            uploadTask = fileReference.putFile(imageUri);
+                            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                                @Override
+                                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                                    if(!task.isSuccessful()){
+                                        throw task.getException();
+                                    }
+
+                                    return fileReference.getDownloadUrl();
+                                }
+                            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Uri> task) {
+
+                                    if (task.isSuccessful()){
+                                        Uri downloadUri = task.getResult();
+                                        String mUri = downloadUri.toString();
+
+                                        reference = FirebaseDatabase.getInstance().getReference("Users").child(fuser.getUid());
+                                        HashMap<String, Object> map = new HashMap<>();
+                                        map.put("imageURL", mUri);
+                                        reference.updateChildren(map);
+
+
+                                    }else{
+                                        Toast.makeText(getContext(), "Failed!", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+                        }else{
+                            Toast.makeText(getContext(), "No image selected", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+
+                }
+            });
+
+
+
 }
